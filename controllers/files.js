65cddf0374e9c6b171bf14489338b61f7c4e10d7
Christopher Brown <io@henrian.com>
2015-05-01 13:23:30 -0500

    Update to latest pdfi API

--- controllers/files.js
+++ controllers/files.js
@@ -10,9 +10,12 @@ var logger = require('loge');
 var Router = require('regex-router');
 
 var pdfi = require('pdfi');
+var pdfi_PDF = require('pdfi/PDF');
 var pdfi_models = require('pdfi/models');
+var pdfi_font = require('pdfi/font');
+var pdfi_parser_states = require('pdfi/parsers/states');
 
-pdfi.logger.level = logger.level;
+pdfi.setLoggerLevel(logger.level);
 
 var files_dirpath = process.env.UPLOADS;
 
@@ -21,8 +24,7 @@ var R = new Router(function(req, res) {
 });
 
 /** GET /files
-
-Get uploaded files.
+List uploaded files
 */
 R.get(/^\/files$/, function(req, res) {
   fs.readdir(files_dirpath, function(err, filenames) {
@@ -45,8 +47,7 @@ R.get(/^\/files$/, function(req, res) {
 });
 
 /** POST /files
-
-Upload new file.
+Upload new file
 */
 R.post(/^\/files$/, function(req, res) {
   /** formidable.IncomingForm#parse(request: http.IncomingMessage,
@@ -76,7 +77,7 @@ R.post(/^\/files$/, function(req, res) {
   new formidable.IncomingForm({multiples: true})
   .on('fileBegin', function(name, file) {
     file.path = path.join(files_dirpath, file.name);
-    logger.info('fileBegin', name, file);
+    logger.debug('fileBegin', name, file);
   })
   .parse(req, function(err, fields, files) {
     if (err) return res.die(err);
@@ -85,55 +86,59 @@ R.post(/^\/files$/, function(req, res) {
   });
 });
 
-var pdf_cache = {};
-function loadPDF(uri_name, res, callback) {
-  var name = decodeURIComponent(uri_name);
+// per-file routes
+
+var _pdf_cache = {};
+
+var FileR = new Router(function(req, res) {
+  res.status(404).die('No resource at: ' + req.url);
+});
+
+/** GET /files/:name/*
+Set req.pdf for all sub-routes, and turn over to FileR router.
+*/
+R.get(/^\/files\/([^\/]+)($|\/)/, function(req, res, m) {
+  var name = decodeURIComponent(m[1]);
   try {
-    if (!(name in pdf_cache)) {
+    if (!(name in _pdf_cache)) {
       var filepath = path.join(files_dirpath, name);
-      pdf_cache[name] = pdfi.PDF.open(filepath);
+      _pdf_cache[name] = pdfi_PDF.open(filepath);
     }
-    callback(pdf_cache[name]);
+    req.pdf = _pdf_cache[name];
   }
   catch (error) {
     return res.die(error);
   }
-}
+  FileR.route(req, res);
+});
 
 /** GET /files/:name
 */
-R.get(/^\/files\/([^\/]+)$/, function(req, res, m) {
-  var name = decodeURIComponent(m[1]);
-  loadPDF(m[1], res, function(pdf) {
-    res.json({
-      // Angular model updating wants the name here too:
-      name: name,
-      size: pdf.size,
-      trailer: _.omit(pdf.trailer._object, 'ID'), // who needs the ID?
-      cross_references: pdf.cross_references,
-    });
+FileR.get(/^\/files\/([^\/]+)$/, function(req, res, m) {
+  res.json({
+    // Angular model updating wants the name here too:
+    name: decodeURIComponent(m[1]),
+    size: req.pdf.size,
+    trailer: _.omit(req.pdf.trailer._object, 'ID'), // who needs the ID?
+    cross_references: req.pdf.cross_references,
   });
 });
 
 /** GET /files/:name/document
 */
-R.get(/^\/files\/([^\/]+)\/document$/, function(req, res, m) {
-  loadPDF(m[1], res, function(pdf) {
-    var section_names = ['col1', 'col2'];
-    var document = pdf.getDocument(section_names);
-    res.json(document);
-  });
+FileR.get(/^\/files\/([^\/]+)\/document$/, function(req, res) {
+  var section_names = ['col1', 'col2'];
+  var document = req.pdf.getDocument(section_names);
+  res.json(document);
 });
 
 /** GET /files/:name/pages
 */
-R.get(/^\/files\/([^\/]+)\/pages$/, function(req, res, m) {
-  loadPDF(m[1], res, function(pdf) {
-    var raw_pages = pdf.pages.map(function(page) {
-      return page._object;
-    });
-    res.json(raw_pages);
+FileR.get(/^\/files\/([^\/]+)\/pages$/, function(req, res) {
+  var raw_pages = req.pdf.pages.map(function(page) {
+    return page._object;
   });
+  res.json(raw_pages);
 });
 
 /** GET /files/:name/pages/:page_number
@@ -141,124 +146,91 @@ R.get(/^\/files\/([^\/]+)\/pages$/, function(req, res, m) {
 In the user interface, page numbers are 1-based. In the pdf representation,
 they are 0-based.
 */
-R.get(/^\/files\/([^\/]+)\/pages\/(\d+)$/, function(req, res, m) {
-  loadPDF(m[1], res, function(pdf) {
-    var page_number = parseInt(m[2], 10);
+FileR.get(/^\/files\/([^\/]+)\/pages\/(\d+)$/, function(req, res, m) {
+  var page_number = parseInt(m[2], 10);
 
-    // subtract one to change indexing from 1-based to 0-based
-    var page = pdf.pages[page_number - 1];
-    var canvas = page.renderCanvas();
+  // subtract one to change indexing from 1-based to 0-based
+  var page = req.pdf.pages[page_number - 1];
+  var canvas = req.pdf.renderPage(page_number - 1);
 
-    res.json({
-      MediaBox: page.MediaBox,
-      canvas: canvas,
-    });
+  res.json({
+    MediaBox: page.MediaBox,
+    canvas: canvas,
   });
 });
 
 /** GET /files/:name/pages/:page_number/contents
 */
-R.get(/^\/files\/([^\/]+)\/pages\/(\d+)\/contents$/, function(req, res, m) {
-  loadPDF(m[1], res, function(pdf) {
-    var page_number = parseInt(m[2], 10);
+FileR.get(/^\/files\/([^\/]+)\/pages\/(\d+)\/contents$/, function(req, res, m) {
+  var page_number = parseInt(m[2], 10);
 
-    // subtract one to change indexing from 1-based to 0-based
-    var page = pdf.pages[page_number - 1];
-    var Contents = page.joinContents('\n'); // returns a string
+  // subtract one to change indexing from 1-based to 0-based
+  var page = req.pdf.pages[page_number - 1];
+  var Contents = page.joinContents('\n'); // returns a string
 
-    res.json({
-      Contents: Contents,
-      // tokens: tokens,
-    });
-  });
+  res.json({Contents: Contents});
 });
 
 /** GET /files/:name/objects */
-R.get(/^\/files\/([^\/]+)\/objects$/, function(req, res) {
+FileR.get(/^\/files\/([^\/]+)\/objects$/, function(req, res) {
   res.die('Not yet implemented');
 });
 
-/** GET /files/:name/objects/:object_id?generation:number=0
-*/
-R.get(/^\/files\/([^\/]+)\/objects\/(\d+)(\?.+|$)/, function(req, res, m) {
-  var urlObj = url.parse(req.url, true);
-  loadPDF(m[1], res, function(pdf) {
-    var object_number = parseInt(m[2], 10);
-    var generation_number = parseInt(urlObj.query.generation || 0, 10);
-
-    var object = pdf.getObject(object_number, generation_number);
-
-    if (pdfi_models.ContentStream.isContentStream(object)) {
-      var content_stream = new pdfi_models.ContentStream(pdf, object);
-      var decoded_object = _.clone(object);
-      decoded_object.buffer = content_stream.buffer;
-      return res.json(decoded_object);
-    }
-
-    res.json(object);
-  });
+var ObjectR = new Router(function(req, res) {
+  res.status(404).die('No resource at: ' + req.url);
 });
 
-/** GET /files/:name/objects/:object_id/extra?generation:number=0
+/** GET /files/:name/objects/:object_number*?generation:number=0
+Set req.object on all sub-routed requests.
 */
-R.get(/^\/files\/([^\/]+)\/objects\/(\d+)\/extra(\?.+|$)/, function(req, res, m) {
+FileR.get(/^\/files\/([^\/]+)\/objects\/(\d+)(\/|\?|$)/, function(req, res, m) {
   var urlObj = url.parse(req.url, true);
-  loadPDF(m[1], res, function(pdf) {
-    var object_number = parseInt(m[2], 10);
-    var generation_number = parseInt(urlObj.query.generation || 0, 10);
+  var object_number = parseInt(m[2], 10);
+  var generation_number = parseInt(urlObj.query.generation || 0, 10);
 
-    // getObject returns the cached object from the pdf lib -- don't modify it!
-    var object = pdf.getObject(object_number, generation_number);
+  // getObject returns the cached object from the pdf lib -- don't modify it!
+  req.object = req.pdf.getObject(object_number, generation_number);
+  ObjectR.route(req, res);
+});
 
-    if (pdfi_models.ContentStream.isContentStream(object)) {
-      var content_stream = new pdfi_models.ContentStream(pdf, object);
+/** GET /files/:name/objects/:object_number?generation:number=0
+*/
+ObjectR.get(/^\/files\/([^\/]+)\/objects\/(\d+)(\?|$)/, function(req, res) {
+  var object = req.object;
+  if (pdfi_models.ContentStream.isContentStream(object)) {
+    var content_stream = new pdfi_models.ContentStream(req.pdf, object);
+    var decoded_object = _.clone(object);
+    decoded_object.buffer = content_stream.buffer;
+    object = decoded_object;
+  }
+  res.json(object);
+});
 
-      var decoded_object = _.clone(object);
-      decoded_object.buffer = content_stream.buffer;
+/** GET /files/:name/objects/:object_number/content-stream?generation:number=0
+*/
+ObjectR.get(/\/content-stream/, function(req, res) {
+  var object = req.object;
 
-      if (content_stream.dictionary.Type == 'XObject' && content_stream.dictionary.Subtype == 'Form') {
-        var canvas = new pdfi.drawing.Canvas(content_stream.dictionary.BBox);
-        var stream_string = content_stream.buffer.toString('binary');
-        var stream_string_iterable = new lexing.StringIterator(stream_string);
-        try {
-          canvas.render(stream_string_iterable, content_stream.Resources);
-        }
-        catch (exception) {
-          return res.json({object: decoded_object, error: exception.stack});
-        }
-        return res.json({object: decoded_object.dictionary, canvas: canvas, buffer: decoded_object.buffer});
-      }
+  var content_stream = new pdfi_models.ContentStream(req.pdf, object);
+  var decoded_object = _.clone(object);
+  decoded_object.buffer = content_stream.buffer;
+  object = decoded_object;
 
-      return res.json({object: decoded_object});
-    }
-    else if (object['Type'] === 'Font') {
-      if (object['Subtype'] === 'Type0') {
-        var type0Font = new pdfi_models.Type0Font(pdf, object);
-        type0Font._initializeWidthMapping();
-        return res.json({object: object, Mapping: _.extend({}, type0Font._widthMapping)});
-      }
-      else if (object['Subtype'] === 'Type1') {
-        var type1Font = new pdfi_models.Type1Font(pdf, object);
-        type1Font._initializeWidthMapping();
-        return res.json({object: object, Mapping: type1Font._widthMapping});
-      }
-      else if (object['Subtype'] === 'TrueType') {
-        var trueTypeFont = new pdfi_models.Type1Font(pdf, object);
-        trueTypeFont._initializeWidthMapping();
-        return res.json({object: object, Mapping: trueTypeFont._widthMapping});
-      }
-    }
-    else if (pdfi_models.Font.isFont(object)) {
+  // TODO: wrap in a try-catch
+  var stream_string = content_stream.buffer.toString('binary');
+  var stream_string_iterable = new lexing.StringIterator(stream_string);
+  var operations = new pdfi_parser_states.CONTENT_STREAM(stream_string_iterable, 1024).read();
 
+  return res.json({operations: operations});
+});
 
-    }
-    else if (pdfi_models.Encoding.isEncoding(object)) {
-      var encoding = new pdfi_models.Encoding(pdf, object);
-      return res.json({object: object, Mapping: encoding.Mapping});
-    }
-
-    res.json(object);
-  });
+/** GET /files/:name/objects/:object_number/font?generation:number=0
+*/
+ObjectR.get(/\/font/, function(req, res) {
+  var object = req.object;
+  var font_Model = new pdfi_models.Model(req.pdf, object);
+  var font = pdfi_font.Font.fromModel(font_Model);
+  res.json({Mapping: font.encodingMapping || null});
 });
 
 module.exports = function(req, res) {
